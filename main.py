c65af65ccec5cdd3666236577c68ae286c43f899
Luci <soaresluciano85@gmail.com>
2024-09-04 22:45:55 -0300

    Add: Adicionado método para buscar no ERP Sankhya instâncias à serem analisadas

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-s = Sankhya(os.getenv('SANKHYA_ID_USERNAME')
+s = Sankhya(  os.getenv('SANKHYA_ID_USERNAME')
             , os.getenv('SANKHYA_PASSWORD')
             , os.getenv('SANKHYA_TOKEN')
             , os.getenv('SANKHYA_APPKEY'))
 s.login()
+instanciasFlow = s.buscaTarefasFlow()
+for i in range(len(instanciasFlow)):
+    print('Instância: '   + str(instanciasFlow[i][0]))
+    print('Solicitação: ' + instanciasFlow[i][1])
+    print('Solicitante: ' + instanciasFlow[i][2])
+    print('Dono: '        + instanciasFlow[i][3])
+    print('Tarefa: '      + instanciasFlow[i][4], end='\n\n')
 s.logout()
--- sankhya.py
+++ sankhya.py
@@ -148,6 +148,178 @@ class Sankhya:
 
         requests.request("GET", url, headers=headers)
 
+    def buscaTarefasFlow(self):
+        """
+        Realiza logout no sistema sankhya, invalidando o token utilizado nas requisições.
+        Busca as instâncias no sistema sankhya as instâncias/protocolo que serão analisadas.
+
+        Retorno da API em caso de sucesso:
+        {
+            "serviceName": "DbExplorerSP.executeQuery",
+            "status": "1",
+            "pendingPrinting": "false",
+            "transactionId": "5BE354E79262E9AA867471BDB69AAE88",
+            "responseBody": {
+                "fieldsMetadata": [
+                    {
+                        "name": "INSTANCIA",
+                        "description": "INSTANCIA",
+                        "order": 1,
+                        "userType": "I"
+                    },
+                    {
+                        "name": "SOLICITACAO",
+                        "description": "SOLICITACAO",
+                        "order": 2,
+                        "userType": "S"
+                    },
+                    {
+                        "name": "SOLICITANTE",
+                        "description": "SOLICITANTE",
+                        "order": 3,
+                        "userType": "S"
+                    },
+                    {
+                        "name": "DONO",
+                        "description": "DONO",
+                        "order": 4,
+                        "userType": "S"
+                    },
+                    {
+                        "name": "TAREFA",
+                        "description": "TAREFA",
+                        "order": 5,
+                        "userType": "S"
+                    },
+                    {
+                        "name": "NOMEPROCESSO",
+                        "description": "NOMEPROCESSO",
+                        "order": 6,
+                        "userType": "S"
+                    }
+                ],
+                "rows": [
+                    [
+                        123,
+                        "Criação de expositor 1",
+                        "Criação de expositor 1",
+                        "Solicitante 1",
+                        "Dono 1",
+                        "Tarefa Flow",
+                        "Processo Flow"
+                    ],
+                    [
+                        456,
+                        "Criação de expositor 2",
+                        "Solicitante 2",
+                        "Dono 2",
+                        "Tarefa Flow",
+                        "Processo Flow"
+                    ],
+                    [
+                        789,
+                        "Criação de expositor 3",
+                        "Solicitante 3",
+                        "Dono 3",
+                        "Tarefa Flow",
+                        "Processo Flow"
+                    ],
+                    [
+                        987,
+                        "Solicito impressão do certificado 'mandou bem'",
+                        "Solicitante 4",
+                        "Dono 4",
+                        "Tarefa Flow",
+                        "Processo flow"
+                    ]
+                ],
+                "burstLimit": false,
+                "timeQuery": "6,251s",
+                "timeResultSet": "1ms"
+            }
+        }
+
+        Retorno da API em caso de falha no login:
+        -Token expirado
+            {
+                "serviceName": "DbExplorerSP.executeQuery",
+                "status": "3",
+                "pendingPrinting": "false",
+                "transactionId": "CC8DEFBD6A98621BFB56F7A5AF3A239A",
+                "statusMessage": "Não autorizado."
+            }
+        
+        - Token invalido: 
+            {
+                "fieldSet": null,
+                "paginate": null,
+                "error": {
+                    "codigo": "GTW3403",
+                    "descricao": "Bearer Token inválido ou Expirado."
+                }
+            }
+        
+        - Comando sql invalido (Erro ao executar o código SQL)
+        {
+            "serviceName": "DbExplorerSP.executeQuery",
+            "status": "0",
+            "pendingPrinting": "false",
+            "transactionId": "4632B24428FEBA6A32CF6578D2E258EA",
+            "statusMessage": mensagem_de_erro(varia de erro para erro)
+        }
+        """
+        url = 'https://api.sankhya.com.br/gateway/v1/mge/service.sbr?serviceName=DbExplorerSP.executeQuery&outputType=json'
+        body = json.dumps({
+            "serviceName": "DbExplorerSP.executeQuery",
+            "requestBody": {
+                "sql": """SELECT pdv.idinstprn AS instancia
+                        , TO_CHAR(pdv.objet) AS solicitacao
+                        , NVL(INITCAP(parsolic.nomeparc), INITCAP(ususolic.nomeusu)) AS solicitante
+                        , NVL(INITCAP(pardono.nomeparc), INITCAP(usudono.nomeusu)) AS dono
+                        , ele.nome AS tarefa
+                        , 'Solicitação de PDV' AS nomeProcesso
+                    FROM twfitar tar
+                    INNER JOIN ad_ppdv pdv     ON pdv.idinstprn    = tar.idinstprn
+                    INNER JOIN tsiusu ususolic ON ususolic.codusu  = tar.codususolicitante
+                    LEFT  JOIN tgfpar parsolic ON parsolic.codparc = ususolic.codparc
+                    INNER JOIN tsiusu usudono  ON usudono.codusu   = tar.codusudono
+                    LEFT  JOIN tgfpar pardono  ON pardono.codparc  = usudono.codparc
+                    INNER JOIN twfele ele      ON ele.idelemento   = tar.idelemento
+                    WHERE dhconclusao IS NULL
+                    AND ele.versao = (SELECT MAX(versao) FROM twfele WHERE idelemento = ele.idelemento)
+
+                    UNION ALL
+
+                    SELECT imp.idinstprn AS instancia
+                        , imp.apelido AS solicitacao
+                        , NVL(INITCAP(parsolic.nomeparc), INITCAP(ususolic.nomeusu)) AS solicitante
+                        , NVL(INITCAP(pardono.nomeparc), INITCAP(usudono.nomeusu)) AS dono
+                        , ele.nome AS tarefa
+                        , 'Solicitação de impressão' AS nomeProcesso
+                    FROM twfitar tar
+                    INNER JOIN ad_pimp imp     ON imp.idinstprn = tar.idinstprn
+                    INNER JOIN tsiusu ususolic ON ususolic.codusu  = tar.codususolicitante
+                    LEFT  JOIN tgfpar parsolic ON parsolic.codparc = ususolic.codparc
+                    INNER JOIN tsiusu usudono  ON usudono.codusu   = tar.codusudono
+                    LEFT  JOIN tgfpar pardono  ON pardono.codparc  = usudono.codparc
+                    INNER JOIN twfele ele      ON ele.idelemento   = tar.idelemento
+                    WHERE dhconclusao IS NULL
+                    AND ele.versao = (SELECT MAX(versao) FROM twfele WHERE idelemento = ele.idelemento)"""
+            }
+        })
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + self.__token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=body)
+        response = json.loads(response.text)
+        if response['status'] == '1':
+            return response['responseBody']['rows']
+        else:
+            return []
+
     def setToken(self, token):
         self.__token = token
 
