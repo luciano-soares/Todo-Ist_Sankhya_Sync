9ae4267dd4a9a42e02ca5bb8d3695399e8b76b97
Luci <soaresluciano85@gmail.com>
2024-09-06 16:27:05 -0300

    Modify: Adicionando instâncias no todo-ist somente quando a mesma não se encontra na plataforma

--- main.py
+++ main.py
@@ -18,6 +18,28 @@ try:
 except Exception as error:
     print(error)
 
+
+#inserindo tarefas no todo-ist
+for i in range(len(instanciasFlow)):
+    jaImportada = False
+    instancia = str(instanciasFlow[i][0])
+    for j in range(len(tarefasTodoIst)):
+        if ('Instância ' + instancia) in tarefasTodoIst[j].content:
+            jaImportada = True
+            break
+    
+    if jaImportada == False:
+        print('Inserindo instância ' + instancia)
+        try:
+            task = api.add_task(content="Instância " + str(instanciasFlow[i][0])
+                                , description= "* **Solicitação**: " + instanciasFlow[i][1] + '\n' + 
+                                               "* **Solicitante**: " + instanciasFlow[i][2] + '\n' + 
+                                               "* **Dono**: "        + instanciasFlow[i][3] + '\n' + 
+                                               "* **Tarefa**: "      + instanciasFlow[i][4]
+                                , project_id=os.getenv('TODOIST_PROJECT_ID'))
+        except Exception as error:
+            print(error)
+
 for i in range(len(tarefasTodoIst)):
     print(tarefasTodoIst[i].content)
 
