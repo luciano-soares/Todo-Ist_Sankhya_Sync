cf4d7c971c2cf258d04021c55bbb251abdff3958
Luci <soaresluciano85@gmail.com>
2024-09-06 16:10:34 -0300

    Add: Buscando tarefas do todo-ist

--- main.py
+++ main.py
@@ -11,21 +11,31 @@ s = Sankhya(  os.getenv('SANKHYA_ID_USERNAME')
             , os.getenv('SANKHYA_APPKEY'))
 s.login()
 instanciasFlow = s.buscaTarefasFlow()
-for i in range(len(instanciasFlow)):
-    print('Instância: '   + str(instanciasFlow[i][0]))
-    print('Solicitação: ' + instanciasFlow[i][1])
-    print('Solicitante: ' + instanciasFlow[i][2])
-    print('Dono: '        + instanciasFlow[i][3])
-    print('Tarefa: '      + instanciasFlow[i][4], end='\n\n')
 
-    try:
-        task = api.add_task(content="Instância " + str(instanciasFlow[i][0])
-                            , description= "* **Solicitação**: " + instanciasFlow[i][1] + '\n' + 
-                                           "* **Solicitante**: " + instanciasFlow[i][2] + '\n' + 
-                                           "* **Dono**: "        + instanciasFlow[i][3] + '\n' + 
-                                           "* **Tarefa**: "      + instanciasFlow[i][4]
-                            , project_id=os.getenv('TODOIST_PROJECT_ID'))
-        print(task)
-    except Exception as error:
-        print(error)
-s.logout()
+#buscando tarefas Todo-ist
+try:
+    tarefasTodoIst = api.get_tasks(project_id = os.getenv('TODOIST_PROJECT_ID'))
+except Exception as error:
+    print(error)
+
+for i in range(len(tarefasTodoIst)):
+    print(tarefasTodoIst[i].content)
+
+#for i in range(len(instanciasFlow)):
+#    print('Instância: '   + str(instanciasFlow[i][0]))
+#    print('Solicitação: ' + instanciasFlow[i][1])
+#    print('Solicitante: ' + instanciasFlow[i][2])
+#    print('Dono: '        + instanciasFlow[i][3])
+#    print('Tarefa: '      + instanciasFlow[i][4], end='\n\n')
+#
+#    try:
+#        task = api.add_task(content="Instância " + str(instanciasFlow[i][0])
+#                            , description= "* **Solicitação**: " + instanciasFlow[i][1] + '\n' + 
+#                                           "* **Solicitante**: " + instanciasFlow[i][2] + '\n' + 
+#                                           "* **Dono**: "        + instanciasFlow[i][3] + '\n' + 
+#                                           "* **Tarefa**: "      + instanciasFlow[i][4]
+#                            , project_id=os.getenv('TODOIST_PROJECT_ID'))
+#        print(task)
+#    except Exception as error:
+#        print(error)
+#s.logout()
